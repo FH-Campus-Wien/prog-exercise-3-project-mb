c3cfb2f0d3359f16037135bfbe80a60b32ba7a95
Manuel Fruehstueck <manuel.fruehstueck@stud.fh-campuswien.ac.at>
2022-10-14 20:07:42 +0200

    Solved Task 1

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,5 +1,7 @@
 package at.ac.fhcampuswien;
 
+import java.time.DayOfWeek;
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -12,5 +14,18 @@ public class App {
         // make method calls
         // print their results
         // etc.
+
+        oneMonthCalendar(31, 3);
+    }
+
+    public static void oneMonthCalendar(int dayCnt, int startWeekDay) {
+        for (int i = 1; i < dayCnt + startWeekDay; i++) {
+            if (i >= startWeekDay) {
+                System.out.printf("%1$2s ", (i - startWeekDay) + 1);
+                if (i % 7 == 0 && i < dayCnt + startWeekDay - 1) System.out.println();
+            } else System.out.print("   ");
+        }
+
+        System.out.println();
     }
 }
