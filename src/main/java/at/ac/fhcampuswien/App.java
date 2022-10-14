2e41024be91cb477fc371480255c90cb91c92d99
Manuel Fruehstueck <manuel.fruehstueck@stud.fh-campuswien.ac.at>
2022-10-14 20:46:26 +0200

    DataTransfer PushCommit
    Task 1 solved
    Task 2 work in progress
    Task 3-6 unsolved

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -6,6 +6,10 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+    // final vars
+    static final int m = (int) 2e31;
+    static final int a = 1103515245;
+    static final int c = 12345;
 
     // Implement all methods as public static
 
@@ -16,8 +20,14 @@ public class App {
         // etc.
 
         oneMonthCalendar(31, 3);
+
+        long[] ranNums = lcg(12345);
+        for (long r : ranNums) {
+            System.out.println(r);
+        }
     }
 
+    // TODO: Task 1
     public static void oneMonthCalendar(int dayCnt, int startWeekDay) {
         for (int i = 1; i < dayCnt + startWeekDay; i++) {
             if (i >= startWeekDay) {
@@ -27,5 +37,18 @@ public class App {
         }
 
         System.out.println();
+    }
+
+    // TODO: Task 2
+    public static long[] lcg(long seed) {
+        long[] ranNums = new long[10];
+
+        ranNums[0] = seed;
+
+        for (int i = 1; i < ranNums.length; i++) {
+            ranNums[i] = (a * ranNums[i - 1] + c) % m;
+        }
+
+        return ranNums;
     }
 }
