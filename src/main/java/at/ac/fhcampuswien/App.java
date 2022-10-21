cb50150f6660810ec96993bc4fc7a55d7bac4603
MBGamesStudios <azeigs-github@yahoo.com>
2022-10-21 11:48:33 +0200

    Solved Task 3

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,6 +1,12 @@
 package at.ac.fhcampuswien;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
+    // global vars
+    //static Scanner sc;
+
     // final vars
     static final long m = (long) ((int) 2e31) + 1;
     static final int a = 1103515245;
@@ -14,12 +20,19 @@ public class App {
         // print their results
         // etc.
 
+        //sc = new Scanner(System.in);
+
+        // Task 1
         oneMonthCalendar(31, 3);
 
+        // Task 2
         long[] ranNums = lcg(0);
         for (long r : ranNums) {
             System.out.println(r);
         }
+
+        // Task 3
+        guessingGame(randomNumberBetweenOneAndHundred());
     }
 
     // TODO: Task 1
@@ -45,5 +58,33 @@ public class App {
         }
 
         return ranNums;
+    }
+
+    // TODO: Task 3
+    public static void guessingGame(int numberToGuess) {
+        int playerGuess;
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 10; i++) {
+            do {
+                System.out.printf("Guess number %d: ", i);
+                playerGuess = sc.nextInt();
+            } while (playerGuess < 1 || playerGuess > 100);
+
+            if (playerGuess < numberToGuess && i < 10)
+                System.out.println("The number AI picked is higher than your guess.");
+            else if (playerGuess > numberToGuess && i < 10)
+                System.out.println("The number AI picked is lower than your guess.");
+            else if (playerGuess == numberToGuess) {
+                System.out.println("You won wisenheimer!");
+                return;
+            }
+        }
+
+        System.out.println("You lost! Have you ever heard of divide & conquer?");
+    }
+
+    public static int randomNumberBetweenOneAndHundred() {
+        return new Random().nextInt(1, 101);
     }
 }
